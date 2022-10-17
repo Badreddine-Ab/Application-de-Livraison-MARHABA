041ad6445e96e46d32552a56741981cb03f8c4b3
Badreddine-Ab <badreddine@gmail.com>
2022-10-17 15:07:19 +0100

    adding the register functiona and about to implement JWT

--- Controllers/AuthController.js
+++ Controllers/AuthController.js
@@ -1,13 +1,43 @@
+const User = require('../Models/userModel')
+const bcrypt = require('bcryptjs')
+const apiError = require('../Utils/apiError')
+const { response } = require('express')
 // method : post
 // url : api/auth/login
 // acces : Public
- const Login =  (req,res) => {
+ const Login =  async (req,res) => {
+    const {username, password} = req.body
+    const user = await User.findOne({username}).lean()
     res.status(200).send(req.body)
 }
 
 // Re
-const Register =  (req,res) => {
-    res.status(200).send('this a register function')
+const Register =  async (req,res,next) => {
+    const {username,password: plainTextPassword} = req.body
+    if(!username || typeof username !== 'string'){
+       return next( new apiError(`Invalid Username`,409))
+    }
+    if(!plainTextPassword || typeof plainTextPassword !== 'string'){
+        return next( new apiError(`Invalid password`,409))
+    }
+    if(!plainTextPassword.length < 5){
+        return next( new apiError(`Password too short, should be at least 6 charachters`,409))
+    }
+    
+    const password = await bcrypt.hash(plainTextPassword, 3)
+    try{
+        const response = await User.create({
+            username,
+            password
+        })
+         new apiError(`User created succesfully`,200)
+    }catch(error){
+        if(error.code === 11000){
+            return next( new apiError(`Username already in use`,400))
+        }throw error
+    }
+
+    res.json({ status:'ok'})
 }
 const ForgetPassword =  (req,res) => {
     res.status(200).send('this a Forget Password function')
--- server.js
+++ server.js
@@ -1,4 +1,5 @@
 require('dotenv').config()
+
 const apiError = require('./Utils/apiError')
 const globalError = require('./Middlewares/errorMiddleware')
 const cors = require('cors');
@@ -14,10 +15,13 @@ const app = express()
 
 app.use(cors());
 app.use(express.json())
+const dbConnection = require("./Config/database")
 app.use('/api/auth',router)
 app.use('/api/manager',routerManager)
 app.use('/api/livreure',routerDelivryMen)
 app.use('/api/client',routerClient)
+
+dbConnection();
 
 app.all('*',(req,res,next) => {
     next(new apiError(`Can't find this route: ${req.originalUrl}`,400))
@@ -27,10 +31,20 @@ app.all('*',(req,res,next) => {
 app.use(globalError);
 
 const port = process.env.PORT || 8081
-app.listen(port, (err)=> {
+const server = app.listen(port, (err)=> {
     if(!err){
     console.log(`the port ${port} is running`)
     }else{
         console.log(err)
     }
+})
+
+// Handle errors outside express
+process.on("unhandledRejection",(err)=> {
+    console.error(`UnhandledRejection Errors : ${err.name} | ${err.message}`);
+    server.close(()=> {
+        console.error('Shutting down....')
+        process.exit(1)
+    })
+    
 })
