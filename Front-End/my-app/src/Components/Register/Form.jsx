bb25c6db577f9b8fd3c95704fe7f1909a8df2be7
Badreddine-Ab <badreddine@gmail.com>
2022-11-01 11:56:15 +0100

    receiving the value of login inputs

--- Front-End/my-app/src/Components/Register/Form.jsx
+++ Front-End/my-app/src/Components/Register/Form.jsx
@@ -3,6 +3,7 @@ import {Link,useMatch,useResolvedPath} from "react-router-dom"
 
 export default function Form(props){
 
+    
     return(
         <div className="w-full bg-white rounded-lg shadow dark:border md:mt-0 sm:max-w-md xl:p-0 dark:bg-gray-800 border-gold">
                 <div className="p-6 space-y-4 md:space-y-6 sm:p-8">
--- Front-End/my-app/src/Pages/Login.jsx
+++ Front-End/my-app/src/Pages/Login.jsx
@@ -1,19 +1,31 @@
-import React from "react";
+import React, {useRef, useState} from "react";
 import Header from "../Components/Register/Header"
 import Form from "../Components/Register/Form"
 import Input from "../Components/Register/Input";
 
-
 export default function Login() {
+
+    const [form,setForm] = useState({
+        email:'',
+        password:''
+    })
+
+    const handleChange = e => {
+        setForm({
+           ...form, [e.target.name]: e.target.value
+        })
+      };
+
     return (
         <section className="bg-gray-50 dark:bg-gray-900">
         <div className="flex flex-col items-center justify-center px-6 py-8 mx-auto md:h-screen lg:py-0">
             <Header/>
-            <Form Title="Login" to='/register'>
-            <Input label="Your Email" placeHolder="badr@youcode.com" input="email"/>
-            <Input label="Your Password" placeHolder="*********" input="password"/>
+            <Form Title="Login" to='/register' state={form} setState={setForm} >
+                <Input handleChange={handleChange} state={form.email} setState={setForm} label="Your Email" placeHolder="badr@youcode.com" input="email"/>
+                <Input handleChange={handleChange} state={form.password} setState={setForm} label="Your Password" placeHolder="*********" input="password"/>
             </Form>
         </div>
+    
       </section>
     );
 }
